dc8ffa113cb8c7d141e10a6ee4dcc1742ad9fd55
Dan Retzlaff <glug101gmail.com>
2023-09-20 10:34:15 -0500

    Another test  subprocess dd

--- storagePerformanceTest.py
+++ storagePerformanceTest.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 #
+import subprocess
+
 baseSize = "10G"
 destinationPath = "/Volumes/restricted"
 
@@ -7,6 +9,6 @@ def performance_test(s):
     dd_subprocess = subprocess.run(["/usr/bin/dd", "if=/dev/zero", "of=" + s + "/test.img", "bs=10G", "count=1"], stdout=subprocess.PIPE)
     print(dd_subprocess) 
 
-startTime = time.time()
+# startTime = time.time()
 performance_test(destinationPath)
-endTime = time.time()
+# endTime = time.time()
