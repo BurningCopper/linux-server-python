67611a910203236fa30c1409657612c35902c13a
Dan Retzlaff <glug101gmail.com>
2023-09-20 16:19:29 -0500

    Storage Performance test should work

--- storagePerformanceTest.py
+++ storagePerformanceTest.py
@@ -3,16 +3,23 @@
 import subprocess
 
 baseSize = "1G"
-destinationPath = "/Volumes/restricted/Test"
+# destinationPath = "/Volumes/restricted/Test"
+destinationPath = "."
 
 def performance_test(s):
     d = subprocess.run(["/usr/bin/dd", "if=/dev/zero", "of=" + s + "/test.img", "bs=" + baseSize, "count=1"], stderr=subprocess.PIPE)
     d = d.stderr
-    print(d)
     s = d.decode('utf-8')
+    s = s.rstrip("\n")
     l = s.split("\n")
-    print(l)
+    s = l[-1]
+    return s
 
-# startTime = time.time()
-performance_test(destinationPath)
-# endTime = time.time()
+# Main program begins
+print("Speed test results for " + destinationPath + ":")
+
+for i in range(5):
+    write_speed = performance_test(destinationPath)
+    write_speed_list = write_speed.split(",")
+    write_speed = write_speed_list[-1]
+    print(write_speed)
