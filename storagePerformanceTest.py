945757bd13dc173401fadb09e7abe4e04747a01f
Dan Retzlaff <glug101gmail.com>
2023-09-20 12:34:08 -0500

    Another test  subprocess dd

--- storagePerformanceTest.py
+++ storagePerformanceTest.py
@@ -9,9 +9,9 @@ def performance_test(s):
     d = subprocess.run(["/usr/bin/dd", "if=/dev/zero", "of=" + s + "/test.img", "bs=" + baseSize, "count=1"], stderr=subprocess.PIPE)
     d = d.stdout
     print(d)
-    s = d.decode('utf-8')
-    l = s.split("\n")
-    print(l)
+    # s = d.decode('utf-8')
+    # l = s.split("\n")
+    # print(l)
 
 # startTime = time.time()
 performance_test(destinationPath)
