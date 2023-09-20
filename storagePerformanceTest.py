d4739175d61dbc88d2eec4afbe2eb126d0bb74dc
Dan Retzlaff <glug101gmail.com>
2023-09-20 11:06:57 -0500

    Another test  subprocess dd

--- storagePerformanceTest.py
+++ storagePerformanceTest.py
@@ -8,6 +8,7 @@ destinationPath = "/Volumes/restricted/Test"
 def performance_test(s):
     d = subprocess.run(["/usr/bin/dd", "if=/dev/zero", "of=" + s + "/test.img", "bs=10G", "count=1"], stdout=subprocess.PIPE)
     d = d.stdout
+    print(d)
     s = d.decode('utf-8')
     l = s.split("\n")
     print(l)
