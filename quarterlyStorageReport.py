606a1c906c4077cbf90840ee1155da13423332ec
burningcopper <glug101@gmail.com>
2025-01-23 16:00:20 -0600

    added module to output the current quarter

--- quarterlyStorageReport.py
+++ quarterlyStorageReport.py
@@ -10,9 +10,25 @@ email_server = "palazzo.psychiatry.wisc.edu"
 price_per_t = 120
 email_from = "deretzlaff@wisc.edu"
 email_to = "deretzlaff@wisc.edu" # weitzman2@wisc.edu, iakere@wisc.edu, wempner@wisc.edu, kkern3@wisc.edu, rashton@wisc.edu"
-email_subject = "Q1 " + datetime.datetime.now().strftime("%Y") + " Storage report for MRI-NAS \n"
+email_subject = print_quarter() + " " + datetime.datetime.now().strftime("%Y") + " Storage report for MRI-NAS \n"
 email_body_formatting = ["Content-type:text/html \n<html><font face=\"Courier New, Courier, monospace\">", "</font></html>"]
 email_spacer = "====================\n"
+
+#
+def print_quarter():
+    q1 = {3, 4, 5}
+    q2 = {6, 7, 8}
+    q3 = {9, 10, 11}
+    q4 = {12, 1, 2}
+    if datetime.datetime.now().month in q1:
+        q = "Q1"
+    if datetime.datetime.now().month in q2:
+        q = "Q2"
+    if datetime.datetime.now().month in q3:
+        q = "Q3"
+    if datetime.datetime.now().month in q4:
+        q = "Q4"
+    return(q)
 
 # Take the input string (s), convert it to a list (l), and outputs a string (s) that contains only the lines that contain the grep search term (g)
 def grep_lines(s, g):
@@ -24,7 +40,7 @@ def grep_lines(s, g):
     return s
 
 # Take the input mount name (drive_in) and output the space used in bytes
-def df_value(drive_in): #apparently this is getting zero hits
+def df_value(drive_in):
     df_subprocess = subprocess.run(["/usr/bin/df", "--output=used,target"], stdout=subprocess.PIPE)
     df_subprocess = df_subprocess.stdout
     size_out = df_subprocess.decode('utf-8')
@@ -58,11 +74,12 @@ def quarterly_price(storage_used):
 # Main program
 storage_report = "Please find below the " + email_subject + "\n"
 
-for i in input_volumes:
-    storage_report = storage_report + email_spacer + "Storage Report for " + i + ":\n"
-    storage_report = storage_report + df_value_human(i) + " Used * $" + str(price_per_t) + "/TB/Year * .25 = $" + str(round(quarterly_price(df_value(i)),2)) + "\n\n"
+# for i in input_volumes:
+#     storage_report = storage_report + email_spacer + "Storage Report for " + i + ":\n"
+#     storage_report = storage_report + df_value_human(i) + " Used * $" + str(price_per_t) + "/TB/Year * .25 = $" + str(round(quarterly_price(df_value(i)),2)) + "\n\n"
 
-print(storage_report)
+# print(storage_report)
+print(print_quarter())
 
 
 
