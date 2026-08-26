e7e2da00d0d246a03cccc72ab84390adaa564846
BurningCopper <deretzlaff@wisc.edu>
2026-08-26 15:12:42 -0500

    added email for report

--- rsyncBackup.py
+++ rsyncBackup.py
@@ -15,7 +15,7 @@ email_body_formatting = ["Content-type:text/html \n<html><font face=\"Courier Ne
 
 # Take the input directory (dir_in) and output the total disk usage for the directory as a string (dir_out)
 def rsync_backup(dir_in, dir_out):
-        rsync_subprocess = subprocess.run(["/usr/bin/rsync", "--recursive", "--archive", "--progress", dir_in, dir_out], stdout=subprocess.PIPE)
+        rsync_subprocess = subprocess.run(["/usr/bin/rsync", "--recursive", "--progress", dir_in, dir_out], stdout=subprocess.PIPE)
         rsync_subprocess = rsync_subprocess.stdout
         rsync_output = rsync_subprocess.decode('utf-8')
         rsync_output = rsync_output.rstrip("\n")
